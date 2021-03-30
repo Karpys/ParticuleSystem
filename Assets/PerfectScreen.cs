3b9b1434850de7c65f6af33379886d52db193131
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-03-30 13:39:01 +0200

    [Feat] Perfect Screen

--- Assets/PerfectScreen.cs
+++ Assets/PerfectScreen.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class ScreenShot : MonoBehaviour
+public class PerfectScreen : MonoBehaviour
 {
     public new Camera camera;
     public int resWidth = 400;
@@ -11,15 +11,17 @@ public class ScreenShot : MonoBehaviour
     public int ScreenPerSec;
     public float RecordTime;
     public float ScreenDelay;
-    public List<byte[]> ListByte=new List<byte[]>();
+    public List<byte[]> ListByte = new List<byte[]>();
     public bool Go;
     public bool Save;
     public float DiviSpeed;
     public int Count;
     private float ScreenDelaySet;
     public string AnimName;
+    private float RecordTimer;
     void Start()
     {
+        RecordTimer = RecordTime;
         GlobalVariable.SpeedDivi = DiviSpeed;
         ScreenDelay = 1 / (float)ScreenPerSec;
         ScreenDelaySet = ScreenDelay;
@@ -36,27 +38,30 @@ public class ScreenShot : MonoBehaviour
         if (Go)
         {
 
-            if (RecordTime > 0)
+            if (ScreenDelay < 0)
             {
-                if (ScreenDelay < 0)
+                RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+                camera.targetTexture = rt;
+                Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.ARGB32, false);
+                camera.Render();
+                RenderTexture.active = rt;
+                screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+                camera.targetTexture = null;
+                RenderTexture.active = null;
+                Destroy(rt);
+                byte[] bytes = screenShot.EncodeToPNG();
+                ListByte.Add(bytes);
+                ScreenDelay = RecordTimer + ScreenDelaySet*3;
+                Debug.Log(ScreenDelay);
+                Count += 1;
+                if(Count>= ScreenPerSec*RecordTimer)
                 {
-                    RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
-                    camera.targetTexture = rt;
-                    Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.ARGB32, false);
-                    camera.Render();
-                    RenderTexture.active = rt;
-                    screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-                    camera.targetTexture = null;
-                    RenderTexture.active = null;
-                    Destroy(rt);
-                    byte[] bytes = screenShot.EncodeToPNG();
-                    ListByte.Add(bytes);
-                    ScreenDelay += ScreenDelaySet;
-                    Count += 1;
+                    Go = false;
                 }
-                ScreenDelay -= Time.fixedDeltaTime;
-                RecordTime -= Time.fixedDeltaTime;
             }
+            ScreenDelay -= Time.fixedDeltaTime;
+            RecordTime -= Time.fixedDeltaTime;
+            
         }
 
         if (Save)
@@ -70,9 +75,4 @@ public class ScreenShot : MonoBehaviour
             Save = false;
         }
     }
-}
-
-public class GlobalVariable
-{
-    public static float SpeedDivi;
 }
